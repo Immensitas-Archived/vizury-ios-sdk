9e40a1ab3950bfff3d5752e17021a64118ebc792
anuragbhowmick <anurag.bhowmick@vizury.com>
2017-10-10 20:15:59 +0530

    adding support for rich media notifications

---
+++ sampleApp/HelloVizury/MyNotiService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  MyNotiService
+//
+//  Created by Anurag on 10/10/17.
+//  Copyright © 2017 Anurag. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
